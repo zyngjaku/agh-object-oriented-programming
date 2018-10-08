ba3fcc6418ce408a27833c877371e61021db8cbd
zyngjaku <zyni98@gmail.com>
2018-10-08 23:28:13 +0200

    Add ex2 (lab1)

---
+++ lab1/WitajSwiecie/witajswiecie.java
@@ -0,0 +1,13 @@
+/**
+ * Klasa wyœwietlajaca powitaniena konsoli.
+ * @author Szymon
+ */
+public class witajswiecie { //Trzeba by?o zmieni? nazw? klasy na tak? jak nazwa pliku, ?eby program dzia?a?
+    /**
+     *
+     * @param argv lista argumentów przekazywana do programu
+     */
+   public static void main(String [] argv){
+       System.out.println("Hello World!");
+  }
+}
